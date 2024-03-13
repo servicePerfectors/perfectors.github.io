542bb1363e8cd83e23fe40deb0dcef1fcf02d3c7
servicePerfectors <160525473+servicePerfectors@users.noreply.github.com>
2024-03-13 20:44:55 +0530

    Delete dist directory

--- dist/assets/use-router-d93a7cf9.js
+++
@@ -1,1 +0,0 @@
-import{ad as o,b as a}from"./index-d7408315.js";function s(){const e=o();return a.useMemo(()=>({back:()=>e(-1),forward:()=>e(1),reload:()=>window.location.reload(),push:r=>e(r),replace:r=>e(r,{replace:!0})}),[e])}export{s as u};
