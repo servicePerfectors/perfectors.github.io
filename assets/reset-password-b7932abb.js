657d1da74a31ae0770e82a51e5de61ef01e1f758
servicePerfectors <160525473+servicePerfectors@users.noreply.github.com>
2024-03-13 20:56:37 +0530

    Add files via upload
    factory reset all the code

---
+++ assets/reset-password-b7932abb.js
@@ -0,0 +1,1 @@
+import{j as s,W as e}from"./index-d7408315.js";function r(){return s.jsx("div",{children:" Reset Passowrd "})}function n(){return s.jsxs(s.Fragment,{children:[s.jsx(e,{children:s.jsx("title",{children:" Reset Password "})}),s.jsx(r,{})]})}export{n as default};
